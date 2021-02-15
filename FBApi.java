3dc7b16bba3bbad9fa9000576828f4ce2e6e9187
nescolindres <44357907+nescolindres@users.noreply.github.com>
2021-02-15 13:31:25 -0800

    Add files via upload

---
+++ FBApi.java
@@ -0,0 +1,31 @@
+package api;
+
+import com.restfb.DefaultFacebookClient;
+import com.restfb.FacebookClient;
+import com.restfb.FacebookClient.AccessToken;
+import com.restfb.Parameter;
+import com.restfb.types.User;
+import com.restfb.Version;
+
+public class FBApi {
+
+	public static void main(String[] args) {
+		
+		String acessToken = "EAAoZAyRkEms0BALy156RZBQpuM0U6DDwBghLN0ZCak7ZBvH4ECadTdyZBEmLh5wIT7kDBZBZCyus7vfJZCeOpbUQyoFE1VrnF3y8gznfuNRehFecOfg7mmjF1nlRIVVNpBU7I03Ax9Mw23f0pwrZAoaVHby737dltVbHSQd3iTbbKtZB0RKTGZCgZANhO3MumzZAj9mDZBZBoq7hgko2svmMuZCamkWU";
+		
+		FacebookClient client = new DefaultFacebookClient(acessToken,Version.LATEST);
+		
+		User me = client.fetchObject("me", User.class, Parameter.with("fields", "birthday,name,email"));
+		
+		System.out.println(me.getName());
+		System.out.println(me.getBirthday());
+		System.out.println(me.getEmail());
+		
+		String MY_APP_ID = "2843101265959629";
+		String MY_APP_SECRET = "42dfe0100b8d7480b3fa2abdac86aeb4";
+		AccessToken accessToken = new DefaultFacebookClient(Version.LATEST).obtainAppAccessToken(MY_APP_ID,MY_APP_SECRET);
+		
+		
+	}
+
+}
